fd5dcc82ad5fb604b26d72296c00f11eddb0b3dc
FrancyDevoLper4 <119678747+FrancyDevoLper4@users.noreply.github.com>
2022-12-02 19:45:11 +0100

    App in kivy
    Kivy e una libreria per creare app

---
+++ Creare app in kivy.py
@@ -0,0 +1,11 @@
+from kivy.app import App
+from kivy.gridlayout import GridLayout 
+class Locator(App):
+    def build(self):
+       self.window = GridLayout
+       self.window.cols = 1 
+       
+       
+       
+      
+   Locator().run()
