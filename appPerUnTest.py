852238869d64e76b7e782a487f82a3cd90e73164
FrancyDevoLper4 <119678747+FrancyDevoLper4@users.noreply.github.com>
2022-12-02 15:02:29 +0100

    Create appPerUnTest.py

---
+++ appPerUnTest.py
@@ -0,0 +1,17 @@
+from kivy.app import App
+from kivy.core.gridlayout import GridLayout
+
+class Prova(App):
+def build(self):
+    self.window = GridLayout
+    self.window.cols =1
+    
+
+
+
+
+
+
+
+
+Prova().run()
