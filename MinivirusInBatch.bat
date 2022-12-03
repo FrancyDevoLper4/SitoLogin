38071ebe1ef2b2a83e9d5a7ae63bdf656d7a4bb3
FrancyDevoLper4 <119678747+FrancyDevoLper4@users.noreply.github.com>
2022-12-03 13:01:36 +0100

    Create MinivirusInBatch.bat

---
+++ MinivirusInBatch.bat
@@ -0,0 +1,29 @@
+@echo off
+
+
+start
+start
+start
+color D
+color F
+cmd shutdown -r -f - 100 echo stai per essere disconnesso il tuo computer verrà riavviato e ripristinato
+del
+pause
+color C
+color 4
+color 2
+color 1
+color 9
+color E
+color D
+echo Hackking
+color 4 echo Hackking Your PC has been hacked now your computer under virus control Say hello to your computer 
+pause
+echo Hacking
+color 2
+
+exit shutdown -r -f -t 100
+
+
+
+
