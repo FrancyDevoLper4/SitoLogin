f2bfe7a4eaa4f42bfd9c1e3c81c4092a031c8e7e
FrancyDevoLper4 <119678747+FrancyDevoLper4@users.noreply.github.com>
2022-12-04 22:21:28 +0100

    Create tkinter.py

---
+++ tkinter.py
@@ -0,0 +1,7 @@
+from Tkinter import tk
+
+root = tk.Tk()
+
+root.title("App Tkinter")
+
+# La seconda parte del codice sorgente uscirà domani spero di farti provare la libreria Tkinter sul computer e Mac 
