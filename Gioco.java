8a86cd12f385befc94ed932aaa0510e84fcbd34d
FrancyDevoLper4 <119678747+FrancyDevoLper4@users.noreply.github.com>
2022-12-04 22:31:56 +0100

    Create Gioco.java

---
+++ Gioco.java
@@ -0,0 +1,22 @@
+package com.gioco.Fortnite;
+
+
+import java.swing*;
+class Gioco{
+
+public stati mai(Strong[] args) {
+private static final int larghezza = 512;
+private static final int altezza = 512;
+
+public gioco{
+
+JFrame frame = new JFrame ( title: "Fortnite");
+
+frame.setSize ( width: 512, height: 512);
+
+frame.setDeafultOperationClose(JFrame.EXIT_ON_CLOSE);
+frame.setVisible(true)
+
+}
+}
+}
